30dd1265c194bcc4831c14acebe437e086f9cf57
amir <amir.naddaf@ut.ac.ir>
2025-01-13 00:45:48 +0330

    Final code

--- Final xv6 in lab5/facttest.c
+++ Final xv6 in lab5/facttest.c
@@ -3,44 +3,48 @@
 
 #define SHM_ID 2
 
-void fact_nfork(int n){
-    int *shm_id = open_sharedmem(SHM_ID);
-    // shm_id[0] = 0;
+void fact_nfork(int n)
+{
+    int *shmem = (int *)open_sharedmem(SHM_ID);
+    shmem[0] = 1;
 
-    for(int i = 0; i < n; i++){
-        if(!fork()){
-            int mult_val = i + 1;
-            // baz krdn
-            // zarb dr meghdare khoone hafeze
-            // bastan
+    for (int i = 0; i < n; i++)
+    {
+        if (!fork())
+        {
+            int *shmem_child = (int *)open_sharedmem(SHM_ID);
+            shmem_child[0] *= (i + 1);
+            close_sharedmem(SHM_ID);
             exit();
         }
-        else 
+        else
             continue;
     }
     for (int i = 0; i < n; i++)
         wait();
-
-    //printf(1, "fact(%d) = %d with %d processes", n, addr[0], n);
-    //syscall close shared mem
+        
+    printf(1, "fact(%d) = %d for %d processes\n", n, shmem[0], n);
+    close_sharedmem(SHM_ID);
 }
 
-void fact_oneprocess(int n){
-    //syscall open shared mem
+void fact_oneprocess(int n)
+{
+    int *shmem = (int *)open_sharedmem(SHM_ID);
+    shmem[0] = 1;
 
-    for(int i = 0; i < n; i++){
-        int mult_val = i + 1;
-        // baz krdn
-        // zarb dr meghdare khoone hafeze
-        // bastan
+    for (int i = 0; i < n; i++)
+    {
+        shmem[0] *= (i + 1);
     }
 
-    //printf(1, "fact(%d) = %d with one process", n, addr[0]);
-    //syscall close shared mem
+    printf(1, "fact(%d) = %d with one processes\n", n, shmem[0], n);
+    close_sharedmem(SHM_ID);
 }
 
-int main(int argc, char *argv[]){
-    if (argc != 2){
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         printf(1, "USAGE: facttest #n");
         exit();
     }
